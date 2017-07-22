4a48cf322cd10075a3422944edc1b7ad7ea31c4e
Stephen Apolinar <stephen.apolinar@outlook.com>
2017-07-22 05:56:05 -0500

    Initial commit of SelectionSortViewer.java from Ch23.6 An Application of Threads.

---
+++ BigJavaTextbook/Multithreading/SelectionSortViewer.java
@@ -0,0 +1,27 @@
+/**Big Java Textbook 2nd edition 
+ * written by Cay Horstmann
+ * Ch23.6 An Application of Threads
+ *
+ * Program constructs GUI
+*/
+import java.awt.BorderLayout;
+import java.awt.JButton;
+import javax.swing.JFrame;
+
+public class SelectionSortComponent {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+
+		final int FRAME_WIDTH = 300;
+		final int FRAME_HEIGHT = 400;
+
+		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		final SelectionSortComponent component = new SelectionSortComponent();
+		frame.add(component, BorderLayout.CENTER);
+
+		frame.setVisible(true);
+		component.startAnimation();
+	}
+}
